c9480925b8657cd726901921900e8687960dd379
Sang Min Park <sangmin.park@gmail.com>
2019-06-22 19:24:31 -0400

    Change <input> to <textarea>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,16 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ViewChild,
-  ElementRef,
-  Renderer2
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  @ViewChild('box') inputBox: ElementRef;
+export class AppComponent implements OnInit {
+  source: string;
 
-  constructor(private renderer: Renderer2) {}
+  constructor() {
+    this.source = '[nomnoml] ls -> [awesome]';
+  }
 
   ngOnInit() {}
-
-  ngAfterViewInit(): void {
-    // Why setTimeout? See
-    // https://blog.angular-university.io/angular-debugging/
-    setTimeout(() => {
-      this.renderer.setProperty(
-        this.inputBox.nativeElement,
-        'value',
-        '[nomnoml] is -> [awesome]'
-      );
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NomnomlComponent } from './nomnoml.component';
 
 @NgModule({
   declarations: [AppComponent, NomnomlComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
